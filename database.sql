b1de9453e163edb16283d6368d720da198897c43
Daniel Andrade <andraddedan@outlook.com>
2020-06-16 10:42:02 -0300

    database file

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS COMENTARIOS (
+  id int NOT NULL AUTO_INCREMENT,
+  comentario text,
+  PRIMARY KEY (id)
+);
+
+insert into COMENTARIOS (comentario) values ('Só sei que através da história a Verdade e o Amor sempre venceram');
+insert into COMENTARIOS (comentario) values ('Este é um pequeno passo para o homem, mas um salto gigantesco para a humanidade');
+insert into COMENTARIOS (comentario) values ('Devemos aceitar a decepção finita, mas nunca perder a esperança infinita');
+insert into COMENTARIOS (comentario) values ('Quem julga as pessoas não tem tempo para amá-las');
+insert into COMENTARIOS (comentario) values ('O conhecimento torna a alma jovem e diminui a amargura da velhice');
